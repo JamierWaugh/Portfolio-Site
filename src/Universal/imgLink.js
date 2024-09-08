1bea96f2c24082fd5f1117b504e9876144c99bb3
JamierWaugh <jamierwaugh@gmail.com>
2024-09-08 20:58:51 +0100

    Fixing errors for deployment

--- src/Universal/imgLink.js
+++ src/Universal/imgLink.js
@@ -1,6 +1,6 @@
 function ImgLink(props) {
     return (
-        <a href = {props.refer} target = "_blank">
+        <a href = {props.refer} target = "_blank" rel="noreferrer">
         <div>
             <img src={props.source} width = {props.width} height = {props.height} alt = {props.altText}></img>
             <h2> {props.title}</h2>
